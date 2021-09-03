39e39c457e1f2d7f2de4ad1fd38dc1c33759a604
awesomecoder233 <tommount58@gmail.com>
2021-09-03 10:57:02 +0200

    fix resouce router

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,7 +11,7 @@ class TaskController extends Controller {
 		$this->middleware('auth');
 	}
 
-	public function index(Request $request, Task $task) {
+	public function index() {
 		// get all the tasks based on current user id
 		$allTasks = $task->whereIn('user_id', $request->user())->with('user');
 		$tasks = $allTasks->orderBy('created_at', 'desc')->take(10)->get();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::resource('/tasks', TaskController::class);
 Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('tasks', '\App\Http\Controllers\TaskController');
+
 
 
